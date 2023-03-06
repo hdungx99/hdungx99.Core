338401780b80f1db4131595c24a570cb8405cb35
Dung Hoang <dane.dung@itlvn.com>
2023-03-06 13:54:55 +0700

    init Core

---
+++ hdungx99.Core.EF/Context/hdungx99Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hdungx99.Core.EF.Context
+{
+    public class hdungx99Context : DbContext
+    {
+    }
+}
